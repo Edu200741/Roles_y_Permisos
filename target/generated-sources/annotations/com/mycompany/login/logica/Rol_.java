ffaa4fea50643b23692096e3d1364dbc90e469b4
Edu200741 <edu200741@gmail.com>
2024-03-27 22:29:18 -0300

    creation .jar

--- target/generated-sources/annotations/com/mycompany/login/logica/Rol_.java
+++ target/generated-sources/annotations/com/mycompany/login/logica/Rol_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-03-26T22:19:58", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-03-27T22:25:18", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Rol.class)
 public class Rol_ { 
 
